9fa721bd6d0517b930f6933c8884c9870174ab85
MarvRoldan <marvinroldan_@hotmail.com>
2020-04-02 22:28:23 -0600

    Reworked with TS syntax

---
+++ main.js
@@ -0,0 +1,35 @@
+var list = ["crossing", "organ", "past", "green", "insight"];
+var randomWord = list[Math.floor(Math.random() * list.length)];
+var letterCorrect = [];
+var i;
+function start() {
+    for (i = 0; i < randomWord.length; i++) {
+        letterCorrect[i] = "__";
+    }
+    document.getElementById("hidden").innerHTML = letterCorrect.join(" ");
+    console.log(randomWord);
+}
+function letterCheck() {
+    var letterGuess;
+    var letterWrong = [];
+    var match = false;
+    document.onkeyup = function (event) {
+        letterGuess = event.key.toLowerCase();
+        for (i = 0; i < randomWord.length; i++) {
+            if (letterGuess === randomWord[i]) {
+                letterCorrect[i] = letterGuess;
+                document.getElementById("hidden").innerHTML = letterCorrect.join(" ");
+                match = true;
+            }
+        }
+        if (match)
+            return;
+        if (letterWrong.indexOf(letterGuess) < 0) {
+            letterWrong.push(letterGuess);
+            document.getElementById("wrongGuess").innerHTML = letterWrong.join(" ");
+        }
+    };
+}
+start();
+letterCheck();
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,40 @@
+const list: string[] = ["crossing", "organ", "past", "green", "insight"];
+const randomWord: string = list[Math.floor(Math.random() * list.length)];
+const letterCorrect: string[] = [];
+let i;
+
+function start(): void {
+  for (i = 0; i < randomWord.length; i++) {
+    letterCorrect[i] = "__";
+  }
+  document.getElementById("hidden").innerHTML = letterCorrect.join(" ");
+  console.log(randomWord);
+}
+
+function letterCheck(): void {
+  let letterGuess;
+  const letterWrong: string[] = [];
+  let match: boolean = false;
+
+  document.onkeyup = event => {
+    letterGuess = event.key.toLowerCase();
+
+    for (i = 0; i < randomWord.length; i++) {
+      if (letterGuess === randomWord[i]) {
+        letterCorrect[i] = letterGuess;
+        document.getElementById("hidden").innerHTML = letterCorrect.join(" ");
+        match = true;
+      }
+    }
+
+    if (match) return;
+
+    if (letterWrong.indexOf(letterGuess) < 0) {
+      letterWrong.push(letterGuess);
+      document.getElementById("wrongGuess").innerHTML = letterWrong.join(" ");
+    }
+  };
+}
+
+start();
+letterCheck();
